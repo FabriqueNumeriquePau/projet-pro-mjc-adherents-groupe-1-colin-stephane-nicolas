c9b849f9d377b18db43cc7acfea472800150ca92
nicodarrieu <nicolasdarrieu@gmail.com>
2021-05-26 15:00:07 +0200

    avancement

--- newsletter.php
+++ newsletter.php
@@ -2,10 +2,14 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="./Assets/css/newsletter.css">
     <link rel="stylesheet" href="./Assets/css/sidemenu.css">
+    <link rel="stylesheet" href="./Assets/css/navmenu.css">
     <link rel="stylesheet" href="./Assets/css/trix.css">
-
+    <title>Newsletter</title>
 </head>
 
 <body>
